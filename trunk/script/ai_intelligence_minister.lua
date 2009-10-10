ba2ebac7807951175b413835a140a94ba0e37d2a
elias.holzer <elias.holzer@2ee7eb82-aa46-11de-8882-bb5941c2e707>
2009-10-10 15:50:20 +0000

    New: Countries will now raise their national unity if it's below 60 in peace time or lower their neutrality otherwise. If they're at war they'll switch to counterespionage. Bugfix: Vanilla function PickBestMission crashed for countries belonging to a faction (like GER, SOV, ENG, FRA, ...) which prevented the AI from doing any intel missions.
    Did also some code cleanup.

--- trunk/script/ai_intelligence_minister.lua
+++ trunk/script/ai_intelligence_minister.lua
@@ -3,35 +3,41 @@
 -----------------------------------------------------------
 
 require('ai_diplomacy')
+require('helper_functions')
 
 function IntelligenceMinister_Tick(minister)
-	if math.mod( CCurrentGameState.GetAIRand(), ai_configuration.INTELLIGENCE_DELAY) == 0 then
+	--if math.mod( CCurrentGameState.GetAIRand(), ai_configuration.INTELLIGENCE_DELAY) == 0 then
 		local ministerTag = minister:GetCountryTag()
 		local ministerCountry = minister:GetCountry()
-		local ai = minister:GetOwnerAI() 
+		local ai = minister:GetOwnerAI()
+
 		ManageSpiesAtHome(minister, ministerTag, ministerCountry, ai)
 		ManageSpiesAbroad(minister, ministerTag, ministerCountry, ai)
-	end
+	--end
 end
 
 function ManageSpiesAtHome(minister, ministerTag, ministerCountry, ai)
 	local domesticSpyPresence = ministerCountry:GetSpyPresence( ministerTag )
 	local currentMission = domesticSpyPresence:GetMission()
-	
-	local nPrio = 1
-	if ministerCountry:IsAtWar() or ministerCountry:GetStrategy():IsPreparingWar() then
-		if not (currentMission == SpyMission.SPYMISSION_COUNTER_ESPIONAGE) then
-			local command = CChangeSpyMission( ministerTag, ministerTag, SpyMission.SPYMISSION_COUNTER_ESPIONAGE )
-			ai:Post( command )
-		end
-		nPrio = nPrio + 2
+	local newMission = SpyMission.SPYMISSION_NONE
+
+	if ministerCountry:IsAtWar() or ministerCountry:GetStrategy():IsPreparingWar() or ministerCountry:IsGovernmentInExile() then
+		newMission = SpyMission.SPYMISSION_COUNTER_ESPIONAGE
+	elseif ministerCountry:GetNationalUnity():Get() < 60 then
+		newMission = SpyMission.SPYMISSION_RAISE_NATIONAL_UNITY
+	else
+		newMission = SpyMission.SPYMISSION_LOWER_NEUTRALITY
 	end
 
-	nPrio = math.min( nPrio, CSpyPresence.MAX_SPY_PRIORITY )
-	local oldPrio = domesticSpyPresence:GetPriority()
-	if not ( oldPrio == nPrio ) then
-		local command = CChangeSpyPriority( ministerTag, ministerTag, nPrio )
-		ai:Post( command )
+	if newMission ~= currentMission then
+		local command = CChangeSpyMission(ministerTag, ministerTag, newMission)
+		ai:Post(command)
+	end
+
+	-- Always go for max priority
+	if domesticSpyPresence:GetPriority() < CSpyPresence.MAX_SPY_PRIORITY then
+		command = CChangeSpyPriority(ministerTag, ministerTag, CSpyPresence.MAX_SPY_PRIORITY)
+		ai:Post(command)
 	end
 
 end
@@ -39,7 +45,7 @@ end
 function PickBestMission(country, minister, ministerTag, ministerCountry, ai)
 
 	local countryTag = country:GetCountryTag()
-	local bestMission = nil
+	local bestMission = SpyMission.SPYMISSION_NONE
 	local bestScore = 0
 
 	if ministerCountry:IsGovernmentInExile() then
@@ -49,54 +55,54 @@ function PickBestMission(country, minister, ministerTag, ministerCountry, ai)
 			bestMission = SpyMission.SPYMISSION_LOWER_NATIONAL_UNITY
 		end
 	else
-		local dislike = 0	
+		local dislike = 0
 		-- at war?
-		if ministerCountry:GetStrategy():IsPreparingWarWith( countryTag ) or 
+		if ministerCountry:GetStrategy():IsPreparingWarWith( countryTag ) or
 		   ministerCountry:GetRelation(countryTag):HasWar() then
 			score = 51
 			if score > bestScore then
 				bestMission = SpyMission.SPYMISSION_MILITARY
 				bestScore = score
 			end
-			
+
 			dislike = 100
 		end
-		
+
 		local rel = ministerCountry:GetRelation( countryTag )
 		if rel:GetValue():Get() < -75  then
 			dislike = dislike + 50
 		end
-		
+
 		dislike = dislike + rel:GetThreat():Get() * CalculateAlignmentFactor( ai, ministerCountry, country )
-	
+
 		local strategy = ministerCountry:GetStrategy()
 		dislike = dislike - strategy:GetFriendliness(countryTag) / 4
 		dislike = dislike + strategy:GetAntagonism(countryTag) / 4
 
-		if ministerCountry:HasFaction() and 
-		   ministerCountry:GetFaction() == countryTag:GetFaction() then
+		if ministerCountry:HasFaction() and
+		   ministerCountry:GetFaction() == country:GetFaction() then
 			dislike = 0
 		end
 
 		if dislike > 50 then
-			local score = 0.0	
-			if country:IsAtWar() then	
-				local surrenderScore = country:GetSurrenderLevel():Get(); 
+			local score = 0.0
+			if country:IsAtWar() then
+				local surrenderScore = country:GetSurrenderLevel():Get();
 				score = surrenderScore * 100
-				
+
 				if score > bestScore then
 					bestMission = SpyMission.SPYMISSION_LOWER_NATIONAL_UNITY
 					bestScore = score
 				end
 			end
-			
+
 			score = country:GetGlobalModifier():GetValue(CModifier._MODIFIER_PARTISAN_EFFICENCY_):Get() + 30
 			if score > bestScore then
 				bestMission = SpyMission.SPYMISSION_SUPPORT_RESISTANCE
 				bestScore = score
 			end
 
-			
+
 			-- are they about to have a coup we want part in?
 			if country:GetDissent():Get() > 5 then
 				local ideology = country:GetRulingIdeology()
@@ -105,8 +111,8 @@ function PickBestMission(country, minister, ministerTag, ministerCountry, ai)
 					local popularity = country:AccessIdeologyPopularity():GetValue( ideology ):Get()
 					local organization = country:AccessIdeologyOrganization():GetValue( ideology ):Get()
 					if popularity < 75 and organization < 75 then
-						
-						local nationalUnity = country:GetNationalUnity():Get()	
+
+						local nationalUnity = country:GetNationalUnity():Get()
 						if nationalUnity >= 80 and nationalUnity < 90 then
 							local score = 50 + (10 - (nationalUnity - 80)) * 3
 							if score > bestScore then
@@ -122,11 +128,11 @@ function PickBestMission(country, minister, ministerTag, ministerCountry, ai)
 								bestScore = score
 							end
 						end
-						
+
 					end
 				end
 			end
-			
+
 			if bestScore < 50 then
 				if math.mod( CCurrentGameState.GetAIRand(), 2) == 0 then
 					bestMission = SpyMission.SPYMISSION_DISRUPT_PRODUCTION
@@ -139,60 +145,63 @@ function PickBestMission(country, minister, ministerTag, ministerCountry, ai)
 		elseif dislike < 40 then
 			bestMission = SpyMission.SPYMISSION_NONE
 			bestScore = 80
-		end	
-		
-	end 
-	
-	
+		end
+	end
+
+
 	if Utils.HasCountryAIFunction( ministerTag, 'PickBestMission' ) then
 		return Utils.CallCountryAI( ministerTag, 'PickBestMission', ai, minister, countryTag, bestMission, bestScore )
 	end
-	
+
 	if bestScore > 50 then
 		return bestMission
 	else
-		return nil
+		return SpyMission.SPYMISSION_NONE
 	end
 end
 
 function ManageSpiesAbroad(minister, ministerTag, ministerCountry, ai)
-	
 	for country in CCurrentGameState.GetCountries() do
 		local tag = country:GetCountryTag()
-		if (not (tag == ministerTag)) and country:Exists() then
-			
-			local SpyPresence = ministerCountry:GetSpyPresence( tag )
+
+		if tag ~= ministerTag then
+			local SpyPresence = ministerCountry:GetSpyPresence(tag)
 			local nPrio = 0
-			if ministerCountry:IsMajor() and country:IsMajor() then
-				nPrio = nPrio + 1
-			end
-			
-			if ministerCountry:GetActingCapitalLocation():GetContinent() == country:GetActingCapitalLocation():GetContinent() then
-				nPrio = nPrio + 1
+			local mission = SpyMission.SPYMISSION_NONE
+
+			if IsValidCountry(country) then
+				if ministerCountry:IsMajor() and country:IsMajor() then
+					nPrio = nPrio + 1
+				end
+
+				if ministerCountry:GetActingCapitalLocation():GetContinent() == country:GetActingCapitalLocation():GetContinent() then
+					nPrio = nPrio + 1
+				end
+
+				if ministerCountry:GetFaction():IsValid() and not (ministerCountry:GetFaction() == country:GetFaction()) and country:GetFaction():IsValid() then
+					nPrio = nPrio + 1
+				end
+
+				if ministerCountry:GetRelation(tag):HasWar() then
+					nPrio = nPrio + 2
+				end
+
+				nPrio = math.min( nPrio, CSpyPresence.MAX_SPY_PRIORITY )
+
+				if nPrio > 0 then
+					mission = PickBestMission(country, minister, ministerTag, ministerCountry, ai)
+				end
 			end
 
-			if ministerCountry:GetFaction():IsValid() and not (ministerCountry:GetFaction() == country:GetFaction()) and country:GetFaction():IsValid() then
-				nPrio = nPrio + 1
-			end
-			
-			if ministerCountry:GetRelation(tag):HasWar() then
-				nPrio = nPrio + 2
+			if nPrio ~= SpyPresence:GetPriority() then
+				local command = CChangeSpyPriority(ministerTag, tag, nPrio)
+				ai:Post(command)
 			end
 
-			nPrio = math.min( nPrio, CSpyPresence.MAX_SPY_PRIORITY )
-			
-			local oldPrio = SpyPresence:GetPriority()
-			if not ( oldPrio == nPrio ) then
-				local command = CChangeSpyPriority( ministerTag, tag, nPrio )
-				ai:Post( command )
-			end
-			
-			local mission = PickBestMission(country, minister, ministerTag, ministerCountry, ai)
-			if mission and not( mission == SpyPresence:GetMission() ) then
+			if mission ~= nil and mission ~= SpyPresence:GetMission() then
 				local missionCommand = CChangeSpyMission( ministerTag, tag, mission )
-				ai:Post( missionCommand )
+				ai:Post(missionCommand)
 			end
 		end
 	end
-	
 end
