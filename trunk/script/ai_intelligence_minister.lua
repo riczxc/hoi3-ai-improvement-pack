20e29bb1a16ed84b9d4e66487c827359488d1f09
js.reisender <js.reisender@2ee7eb82-aa46-11de-8882-bb5941c2e707>
2009-10-12 07:46:07 +0000

    Rewritten PickBestMission()

--- trunk/script/ai_intelligence_minister.lua
+++ trunk/script/ai_intelligence_minister.lua
@@ -66,125 +66,120 @@ function ManageSpiesAtHome(minister, ministerTag, ministerCountry, ai)
 
 end
 
-function PickBestMission(country, minister, ministerTag, ministerCountry, ai)
-
+function PickBestMission(country, minister, ministerTag, ministerCountry, ai)	
 	local countryTag = country:GetCountryTag()
-	local bestMission = SpyMission.SPYMISSION_NONE
-	local bestScore = 0
+	--Utils.LUA_DEBUGOUT( tostring(ministerTag).." PickBestMission for "..tostring(countryTag) )
+	local dislike = 0
 
+	-- same faction?
+	if country:HasFaction() and ministerCountry:HasFaction() and ministerCountry:GetFaction() == country:GetFaction()  then
+		--Utils.LUA_DEBUGOUT( tostring(ministerTag).." and "..tostring(countryTag).." in same faction -> SPYMISSION_NONE" )
+		return PickBestMissionCallback( ministerTag, ai, minister, countryTag, SpyMission.SPYMISSION_NONE, 100)
+	end
+	
+	-- enemy controller?
 	if ministerCountry:IsGovernmentInExile() then
 		local capitalController = ministerCountry:GetCapitalLocation():GetController()
-		if not ministerCountry:IsFriend( capitalController, false ) then
-			-- set best straight away
-			bestMission = SpyMission.SPYMISSION_LOWER_NATIONAL_UNITY
+		if not ministerCountry:IsFriend( capitalController, false ) and country==capitalController then
+			--Utils.LUA_DEBUGOUT( tostring(ministerTag).." in exile and occupied by "..tostring(countryTag).." -> SPYMISSION_LOWER_NATIONAL_UNITY" )
+			return PickBestMissionCallback( ministerTag, ai, minister, countryTag, SpyMission.SPYMISSION_LOWER_NATIONAL_UNITY, 100)
 		end
-	else
-		local dislike = 0
-		-- at war?
-		if ministerCountry:GetStrategy():IsPreparingWarWith( countryTag ) or
-		   ministerCountry:GetRelation(countryTag):HasWar() then
-			score = 51
-			if score > bestScore then
-				bestMission = SpyMission.SPYMISSION_MILITARY
-				bestScore = score
-			end
+	end
+	
+	-- at war?
+	if ministerCountry:GetStrategy():IsPreparingWarWith( countryTag ) or
+	   ministerCountry:GetRelation(countryTag):HasWar()
+	then
+		if country:GetSurrenderLevel():Get() > 0.5 then
+			--Utils.LUA_DEBUGOUT( tostring(ministerTag).." and "..tostring(countryTag).." at war -> SPYMISSION_LOWER_NATIONAL_UNITY" )
+			return PickBestMissionCallback( ministerTag, ai, minister, countryTag, SpyMission.SPYMISSION_LOWER_NATIONAL_UNITY, 100)
+		end
+		--Utils.LUA_DEBUGOUT( tostring(ministerTag).." and "..tostring(countryTag).." at war -> SPYMISSION_MILITARY" )
+		return PickBestMissionCallback( ministerTag, ai, minister, countryTag, SpyMission.SPYMISSION_MILITARY, 100)
+	end
+	
+	local rel = ministerCountry:GetRelation( countryTag )
+	local strategy = ministerCountry:GetStrategy()
+	
+	if rel:GetValue():Get() < 0  then
+		dislike = -1*rel:GetValue():Get()/4
+	end
+	
+	dislike = dislike + rel:GetThreat():Get() * CalculateAlignmentFactor( ai, ministerCountry, country )
+	dislike = dislike - strategy:GetFriendliness(countryTag) / 4
+	dislike = dislike + strategy:GetAntagonism(countryTag) / 4
 
-			dislike = 100
+	if dislike > 50 then
+		local bestMission = SpyMission.SPYMISSION_NONE
+		local bestScore = 0
+		local score = 0
+
+		score = country:GetGlobalModifier():GetValue(CModifier._MODIFIER_PARTISAN_EFFICENCY_):Get() + 30
+		if score > bestScore then
+			bestMission = SpyMission.SPYMISSION_SUPPORT_RESISTANCE
+			bestScore = score
 		end
 
-		local rel = ministerCountry:GetRelation( countryTag )
-		if rel:GetValue():Get() < -75  then
-			dislike = dislike + 50
-		end
-
-		dislike = dislike + rel:GetThreat():Get() * CalculateAlignmentFactor( ai, ministerCountry, country )
-
-		local strategy = ministerCountry:GetStrategy()
-		dislike = dislike - strategy:GetFriendliness(countryTag) / 4
-		dislike = dislike + strategy:GetAntagonism(countryTag) / 4
-
-		if ministerCountry:HasFaction() and
-		   ministerCountry:GetFaction() == country:GetFaction() then
-			dislike = 0
-		end
-
-		if dislike > 50 then
-			local score = 0.0
-			if country:IsAtWar() then
-				local surrenderScore = country:GetSurrenderLevel():Get();
-				score = surrenderScore * 100
-
-				if score > bestScore then
-					bestMission = SpyMission.SPYMISSION_LOWER_NATIONAL_UNITY
-					bestScore = score
-				end
-			end
-
-			score = country:GetGlobalModifier():GetValue(CModifier._MODIFIER_PARTISAN_EFFICENCY_):Get() + 30
-			if score > bestScore then
-				bestMission = SpyMission.SPYMISSION_SUPPORT_RESISTANCE
-				bestScore = score
-			end
-
-
-			-- are they about to have a coup we want part in?
-			if country:GetDissent():Get() > 5 then
-				local ideology = country:GetRulingIdeology()
-				local ministerIdeology = ministerCountry:GetRulingIdeology()
-				if not (ideology:GetGroup() == ministerIdeology:GetGroup()) then
-					local popularity = country:AccessIdeologyPopularity():GetValue( ideology ):Get()
-					local organization = country:AccessIdeologyOrganization():GetValue( ideology ):Get()
-					if popularity < 75 and organization < 75 then
-
-						local nationalUnity = country:GetNationalUnity():Get()
-						if nationalUnity >= 80 and nationalUnity < 90 then
-							local score = 50 + (10 - (nationalUnity - 80)) * 3
-							if score > bestScore then
-								bestMission = SpyMission.SPYMISSION_LOWER_NATIONAL_UNITY
-								bestScore = score
-							end
+		-- are they about to have a coup we want part in?
+		if country:GetDissent():Get() > 5 then
+			local ideology = country:GetRulingIdeology()
+			local ministerIdeology = ministerCountry:GetRulingIdeology()
+			if not (ideology:GetGroup() == ministerIdeology:GetGroup()) then
+				local popularity = country:AccessIdeologyPopularity():GetValue( ideology ):Get()
+				local organization = country:AccessIdeologyOrganization():GetValue( ideology ):Get()
+				if popularity < 75 and organization < 75 then
+					local nationalUnity = country:GetNationalUnity():Get()
+					if nationalUnity >= 80 and nationalUnity < 90 then
+						local score = 50 + (10 - (nationalUnity - 80)) * 3
+						if score > bestScore then
+							bestMission = SpyMission.SPYMISSION_LOWER_NATIONAL_UNITY
+							bestScore = score
 						end
-
-						if ministerCountry:AccessIdeologyPopularity():GetValue( ministerIdeology ):Get() > 35 then
-							local score = 50 + (80 - popularity)
-							if score > bestScore then
-								bestMission = SpyMission.SPYMISSION_BOOST_OUR_PARTY
-								bestScore = score
-							end
+					end
+					if ministerCountry:AccessIdeologyPopularity():GetValue( ministerIdeology ):Get() > 35 then
+						local score = 50 + (80 - popularity)
+						if score > bestScore then
+							bestMission = SpyMission.SPYMISSION_BOOST_OUR_PARTY
+							bestScore = score
 						end
-
 					end
 				end
 			end
-
-			if bestScore < 50 then
-				if math.mod( CCurrentGameState.GetAIRand(), 2) == 0 then
-					bestMission = SpyMission.SPYMISSION_DISRUPT_PRODUCTION
-					bestScore = 80
-				else
-					bestMission = SpyMission.SPYMISSION_DISRUPT_RESEARCH
-					bestScore = 80
-				end
-			end
-		elseif dislike < 40 then
-			bestMission = SpyMission.SPYMISSION_NONE
-			bestScore = 80
 		end
+		-- didn't find anything good
+		if bestScore < 50 then
+			if math.mod( CCurrentGameState.GetAIRand(), 2) == 0 then
+				bestMission = SpyMission.SPYMISSION_DISRUPT_PRODUCTION
+			else
+				bestMission = SpyMission.SPYMISSION_DISRUPT_RESEARCH
+			end
+		end
+		return PickBestMissionCallback( ministerTag, ai, minister, countryTag, bestMission, bestScore )
 	end
+	
+	if country:HasFaction() and ministerCountry:HasFaction() and ministerCountry:GetFaction() ~= country:GetFaction() then
+		--Utils.LUA_DEBUGOUT( tostring(ministerTag).." and "..tostring(countryTag).." in different factions -> SPYMISSION_INCREASE_THREAT" )
+		return PickBestMissionCallback( ministerTag, ai, minister, countryTag, SpyMission.SPYMISSION_INCREASE_THREAT, 50 )
+	end
+	if not country:HasFaction() and ministerCountry:HasFaction() then
+		--Utils.LUA_DEBUGOUT( tostring(ministerTag).." mission for "..tostring(countryTag).." is SPYMISSION_BOOST_OUR_PARTY" )
+		return PickBestMissionCallback( ministerTag, ai, minister, countryTag, SpyMission.SPYMISSION_BOOST_OUR_PARTY, 50 )
+	end	
+	--Utils.LUA_DEBUGOUT( tostring(ministerTag).." NO mission for "..tostring(countryTag).." -> SPYMISSION_NONE" )
+	return PickBestMissionCallback( ministerTag, ai, minister, countryTag, SpyMission.SPYMISSION_NONE, 0 )
+end
 
-
+function PickBestMissionCallback( ministerTag, ai, minister, countryTag, SelectedSpyMission, score )
 	if Utils.HasCountryAIFunction( ministerTag, 'PickBestMission' ) then
-		return Utils.CallCountryAI( ministerTag, 'PickBestMission', ai, minister, countryTag, bestMission, bestScore )
-	end
-
-	if bestScore > 50 then
-		return bestMission
+		--Utils.LUA_DEBUGOUT( "-------------- "..tostring(ministerTag).." modified mission "..tostring(countryTag).." ---- " )
+		return Utils.CallCountryAI( ministerTag, 'PickBestMission', ai, minister, countryTag, SelectedSpyMission, score )
 	else
-		return SpyMission.SPYMISSION_NONE
+		return SelectedSpyMission
 	end
 end
 
 function ManageSpiesAbroad(minister, ministerTag, ministerCountry, ai)
+	--Utils.LUA_DEBUGOUT( tostring(ministerTag).." ManageSpiesAbroad ")
 	for country in CCurrentGameState.GetCountries() do
 		local tag = country:GetCountryTag()
 
