4adda54ad2fbef440d0107485ec276d9221533a9
elias.holzer <elias.holzer@2ee7eb82-aa46-11de-8882-bb5941c2e707>
2009-10-31 22:59:02 +0000

    Removed money stock factor in calculation of cg need to adapt to 1.3 beta.

--- trunk/script/ai_production_minister.lua
+++ trunk/script/ai_production_minister.lua
@@ -420,9 +420,6 @@ function BalanceProductionSliders(ai, ministerCountry, prioSelection)
 	-- CONSUMER
 	local ConsumerNeed = ministerCountry:GetProductionDistributionAt( CDistributionSetting._PRODUCTION_CONSUMER_ ):GetNeeded():Get() --/ TotalIC
 
-	-- MONEY
-	local MoneyStockFactor = ministerCountry:GetPool():Get( CGoodsPool._MONEY_ ):Get()/ministerCountry:GetTotalIC()
-
 	-- SUPPLY
 	local SupplyStockFactor = ministerCountry:GetPool():Get( CGoodsPool._SUPPLIES_ ):Get()/math.min(45000, MinStock(ministerCountry, CGoodsPool._SUPPLIES_ ))
 	-- 100% supply prod. at 0 stock, 50% at 1/2 goal stock and 0% at goal stock
@@ -445,7 +442,7 @@ function BalanceProductionSliders(ai, ministerCountry, prioSelection)
 	-- Distribute IC --
 
 	-- consumer need (to prevent new dissent) + boost for dissent or low money stock but never more than 90% of IC
-	ConsumerNeed = ConsumerNeed + math.max(0, MaxIC-ConsumerNeed) * math.min(math.max(dissent/10, 1-MoneyStockFactor), 0.9)
+	ConsumerNeed = ConsumerNeed + math.max(0, MaxIC-ConsumerNeed) * math.min(dissent/10, 0.9)
 
 	-- not more than availIC
 	ConsumerNeed = math.min(availIC, ConsumerNeed)
