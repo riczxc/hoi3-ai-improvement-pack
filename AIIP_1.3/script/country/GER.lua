0b6e010432b8e9f266a3353c0aef57f2e2f00f65
elias.holzer <elias.holzer@2ee7eb82-aa46-11de-8882-bb5941c2e707>
2010-01-18 03:14:08 +0000

    * Start war against France in May not June 1940. * Fix: Canceling the non aggression pact with Soviet Union crashed (bug introduced by vanilla code). This prevented the script from attacking the Soviet Union if non aggression pact was active and it further caused the weird behavior the Germany declared war on DEN/NOR/SWE and BEL/LUX/HOL in the same moment. * Germany will call Italy for help in war against France if the war progress is good. This will help the Vichy event to fire, because national unity of France is higher than in vanilla and so more victory points have to be conquered by Germany in order to get France to surrender. * Once Germany declares war on Soviet Union, which is a limited war, it will call in help by Hungary, Romania and Bulgaria. * Fix: Syntax error prevented script from even being loaded. In last AIIP versions the HOI3 engine fell back to vanilla script (which is also bugged, see comment above). * In order to prepare for Barbarossa, Germany will start to get Romania and Finland into the Axis, if war against France has good progress (should probably start earlier, didn't get them into axis by 1941 in a test game).

--- AIIP_1.3/script/country/GER.lua
+++ AIIP_1.3/script/country/GER.lua
@@ -46,8 +46,10 @@ function P.IsFullyOccupying( ministerCountry, targetTag )
 end
 
 function P.ProposeDeclareWar( minister )
-	--Utils.LUA_DEBUGOUT("ENTER PROPOSE DECLARE WAR GERMANY")
+	-- Utils.LUA_DEBUGOUT("ENTER PROPOSE DECLARE WAR GERMANY")
+
 	local ai = minister:GetOwnerAI()
+	local ministerTag = minister:GetCountryTag()
 	local ministerCountry = minister:GetCountry()
 	local strategy = ministerCountry:GetStrategy()
 	local year = ai:GetCurrentDate():GetYear()
@@ -75,7 +77,7 @@ function P.ProposeDeclareWar( minister )
 	and CCurrentGameState.GetProvince( 2553 ):GetController() == gerTag		--Saarbrucken
 	and CCurrentGameState.GetProvince( 1570 ):GetController() == gerTag		--Wilhelmshaven
 	then
-		--Utils.LUA_DEBUGOUT("GO for Fall Weseruebung")
+		-- Utils.LUA_DEBUGOUT("GO for Fall Weseruebung")
 		if not ministerCountry:GetRelation(denTag):HasWar()						--Not already at war with DEN
 		and not denTag:GetCountry():IsSubject() 								--DEN isn't a subject nation
 		and CCurrentGameState.GetProvince( 1482 ):GetController() == denTag 	--DEN controls Copenhagen
@@ -108,7 +110,7 @@ function P.ProposeDeclareWar( minister )
 				polTag:GetCountry():IsGovernmentInExile()
 			) -- If Poland is no more a problem
 		and (
-				(year > 1939 and month > 5)
+				(year > 1939 and month > 4)
 			or
 				(
 					(
@@ -125,7 +127,7 @@ function P.ProposeDeclareWar( minister )
 				)
 			)
 	then
-		--Utils.LUA_DEBUGOUT("GO for Fall Gelb")
+		-- Utils.LUA_DEBUGOUT("GO for Fall Gelb")
 		if not ministerCountry:GetRelation(holTag):HasWar()						--Not already at war with HOL
 		and not holTag:GetCountry():IsSubject() 								--HOL isn't a subject nation
 		and CCurrentGameState.GetProvince( 1910 ):GetController() == holTag 	--HOL controls Amsterdam
@@ -166,6 +168,7 @@ function P.ProposeDeclareWar( minister )
 	and CCurrentGameState.GetProvince( 3912 ):GetController() == yugTag	--YUG controls Belgrade
 	and vicTag:IsValid() and vicTag:IsReal() and vicTag:GetCountry():Exists() --VIC exists
 	then
+		-- Utils.LUA_DEBUGOUT("GO for Fall Yugoslavia")
 		strategy:PrepareWar( yugTag, 100 )
 	end
 
@@ -190,20 +193,71 @@ function P.ProposeDeclareWar( minister )
 
 		if not AtWarWithNeighbor then
 			if ministerCountry:GetRelation(sovTag):HasNap() then
-				local action = CNapAction(ministerTag, countryTag)
+				local action = CNapAction(ministerTag, sovTag)
 				action:SetValue(false)
 				if action:IsSelectable() then
 					ai:PostAction( action )
 				end
 			end
 
-			--Utils.LUA_DEBUGOUT("GO for Barbarossa")
+			-- Utils.LUA_DEBUGOUT("Go for Barbarossa")
 			strategy:PrepareWar( sovTag, 100 )
 		end
 	end
 
+	-- Utils.LUA_DEBUGOUT("EXIT PROPOSE DECLARE WAR GERMANY")
 end
 
+function P.CheckWar(minister, target, war)
+	-- Utils.LUA_DEBUGOUT("ENTER CHECK WAR GERMANY: " .. tostring(target))
+
+	local ai = minister:GetOwnerAI()
+	local ministerTag = minister:GetCountryTag()
+	local ministerCountry = minister:GetCountry()
+	local strategy = ministerCountry:GetStrategy()
+	
+	local fraTag = CCountryDataBase.GetTag('FRA')
+	local sovTag = CCountryDataBase.GetTag('SOV')
+	
+	if target == fraTag and war:IsLimited() then
+		local itaTag = CCountryDataBase.GetTag('ITA')
+		local vicTag = CCountryDataBase.GetTag('VIC')
+		
+		if itaTag:GetCountry():GetFaction() == ministerCountry:GetFaction()	--Italy is in Axis
+		and not (vicTag:IsValid() and vicTag:IsReal() and vicTag:GetCountry():Exists()) --VIC not exists
+		and fraTag:GetCountry():GetSurrenderLevel():Get() > 0.3 			--We are winning
+		then
+			if not war:IsPartOfWar(itaTag) then
+				local action = CCallAllyAction( ministerTag, itaTag, target )
+				action:SetValue(true) -- limited
+				if action:IsSelectable() then
+					-- Utils.LUA_DEBUGOUT("Call Italy to help against France")
+					ai:PostAction(action)
+				end
+			end
+		end
+	elseif target == sovTag and war:IsLimited() then
+		local allies = {
+			CCountryDataBase.GetTag('HUN'),
+			CCountryDataBase.GetTag('ROM'),
+			CCountryDataBase.GetTag('BUL')
+		}
+		
+		for _,ally in ipairs(allies) do
+			if ally:GetCountry():GetFaction() == ministerCountry:GetFaction()	--Ally is in Axis
+			and not war:IsPartOfWar(ally)
+			then
+				local action = CCallAllyAction(ministerTag, ally, target)
+				action:SetValue(true) -- limited
+				if action:IsSelectable() then
+					ai:PostAction(action)
+				end
+			end
+		end
+	end
+	
+	-- Utils.LUA_DEBUGOUT("EXIT CHECK WAR GERMANY")
+end
 
 function P.PickBestMission(ai, minister, countryTag, bestMission, bestScore )
 	if tostring(countryTag) == 'AUS' then
@@ -212,10 +266,10 @@ function P.PickBestMission(ai, minister, countryTag, bestMission, bestScore )
 		bestMission = SpyMission.SPYMISSION_LOWER_NATIONAL_UNITY
 	elseif tostring(countryTag) == 'FRA' then
 		local fraTag = CCountryDataBase.GetTag('FRA')
-		
-		if minister:GetCountry():GetRelation(fraTag):HasWar()
+
+		if minister:GetCountry():GetRelation(fraTag):HasWar() then
 			local vicTag = CCountryDataBase.GetTag('VIC')
-			
+
 			if not (vicTag:IsValid() and vicTag:IsReal() and vicTag:GetCountry():Exists()) then
 				bestScore = 100
 				bestMission = SpyMission.SPYMISSION_LOWER_NATIONAL_UNITY
@@ -226,17 +280,42 @@ function P.PickBestMission(ai, minister, countryTag, bestMission, bestScore )
 end
 
 function P.DiploScore_InfluenceNation( score, ai, actor, recipient, observer )
--- If Germany didn't take Paris, it must avoid Italy to be influenced by allies faction
+	local romTag = CCountryDataBase.GetTag('ROM')
+	local finTag = CCountryDataBase.GetTag('FIN')
+	local japTag = CCountryDataBase.GetTag('JAP')
 
-	-- when to work on italy.
---	if tostring(recipient) == 'ITA'
---	then
---		if CCurrentGameState.GetProvince( 2613 ):GetController() == actor then -- paris must be ours
---			return	150
---		end
---		return 0
---	end
+	if recipient == romTag or recipient == finTag then
+		local gerTag = CCountryDataBase.GetTag('GER')
+		local fraTag = CCountryDataBase.GetTag('FRA')
+		local vicTag = CCountryDataBase.GetTag('VIC')
 
+		-- Prepare for Barbarossa
+		if (vicTag:IsValid() and vicTag:IsReal() and vicTag:GetCountry():Exists()) 	--VIC exists
+		or (
+			gerTag:GetCountry():GetRelation(fraTag):HasWar() and					--GER is fighting FRA
+			fraTag:GetCountry():GetSurrenderLevel():Get() > 0.5 					--GER is winning
+		)
+		then
+			return 100
+		end
+	-- elseif recipient == japTag then
+		-- local chiTag = CCountryDataBase.GetTag('CHI')
+		
+		-- Wait till they've sorted out their problem with China...
+		-- if 	not (chiTag:IsValid() and chiTag:IsReal() and chiTag:GetCountry():Exists())
+		-- or	(
+			-- chiTag:GetCountry():IsSubject() and chiTag:GetCountry():GetOverlord() == japTag
+		-- )
+		-- or 	(
+			-- chiTag:GetCountry():GetRelation(japTag):HasWar() and
+			-- chiTag:GetCountry():GetSurrenderLevel():Get() > 0.5
+		-- )
+		-- then
+			-- return 100
+		-- else
+			-- return 0
+		-- end
+	end
 
 	return score
 end
