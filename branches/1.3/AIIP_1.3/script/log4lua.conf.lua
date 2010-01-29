02dcac43d6e2d4100282826a54f1feba0c6c6b06
guibod12 <guibod12@2ee7eb82-aa46-11de-8882-bb5941c2e707>
2010-01-29 16:20:21 +0000

    * Some comments and notes in log4lua.conf.lua

--- branches/1.3/AIIP_1.3/script/log4lua.conf.lua
+++ branches/1.3/AIIP_1.3/script/log4lua.conf.lua
@@ -1,3 +1,20 @@
+-- Log file definition
+--
+-- @see http://www.hscale.org/display/LUA/Log4LUA
+--
+-- ROOT category is mandatory as it is the fallback log file
+-- SQL category is reserved for SQLite traces
+-- DEVEL category is great to log information from the function you work on
+-- Other categories are dedicated to ministers
+
+-- Notes:
+-- * Composite loggers are working great
+-- * Log4lua provide an SMTP (mail) support
+-- * Console logger will not work inside Clausewitz engine ! There is no STDOUT.
+
+-- Please consider that log4lua is UNABLE to create directories
+-- You'll may need to create "logs" directory in mod dir for log4lua to work in SciTE editor.
+
 local logger = require("log4lua.logger")
 local console = require("log4lua.appenders.console")
 local file = require("log4lua.appenders.file")
