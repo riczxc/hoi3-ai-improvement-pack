6a93b1d765b6fe4b91ed9b5823df9613d7ab3fc5
guibod@free.fr <guibod@free.fr@2ee7eb82-aa46-11de-8882-bb5941c2e707>
2010-06-16 15:52:40 +0000

    - added ai_strategic.lua to standard inclusions autoexec.lua - Extended Strategic_TerritoryStatus   * CountryTag cache   * Counts provinces   * Provide control ratio   * infolevel parameter saves more resource - Implement flags check in JAP ai   * P.ProductionWeight rush naval/air only if lost continental war or running pacific war with no major involvement on continent (e.g. SOV as enemy)   * P.AirRatio ignore NAV bomber if not running PACIFIC_WAR   * P.EvaluateFlags added with massive comment - Strategic_Evaluate still needs to be called from ForeignMinister, or any other ministers ?

--- script/ai_strategic.lua
+++ script/ai_strategic.lua
@@ -22,60 +22,121 @@ function Strategic_SetFlag(minister, sFlagName, bValue)
 end
 
 -- Are we fighting for key provinces ?
+--
 -- Listed territories are disputed between minister country and 
 -- opposed belligerants (country we are currently at war with or willing to fight)
 --
+-- This function can be resource intensive, piInfoLevel flag allow function caller
+-- to stick define information finess from very basic features to complete report. 
+-- 
+-- INFOLEVEL = 1
+-- All other countries are consider as minors and enemies. 
+-- No data is provided from province except owner.
+-- 
+-- INFOLEVEL = 2
+-- Distinction is provided between neutral, enemies and allies. Countries are checked
+-- as major or not. 
+-- No data is provided from province except owner.
+-- 
+-- INFOLEVEL = 3
+-- Distinction is provided between neutral, enemies and allies. Countries are checked
+-- as major or not. 
+-- Province data ought to be provided but due to lack of LUA support, no information yet.
+-- 
 -- Returns a table defined as :
--- status.isDisputed
--- status.isOutOfControl
--- status.hasOutOfControl
--- status.isUnderControl
--- status.hasUnderControl
--- status.hasMajorInvolved (major ENEMY)
+-- status.numAll            : number,  count of province that really exists
+-- status.isOutOfControl	: boolean, all provinces are owned by enemies (or other country if infolevel =< 1)
+-- status.hasOutOfControl	: boolean, some provinces are owned by enemies (or another country if infolevel =< 1)
+-- status.numOutOfControl	: number,  count of province owned by enemies (or another country if infolevel =< 1)
+-- status.ratioOutOfControl : number,  synonym for numOutOfControl/numAll
+-- status.isUnderControl	: boolean, all  provinces are owned by us (or allied country if infolevel > 1)
+-- status.hasUnderControl	: boolean, some provinces are owned by us (or allied country if infolevel > 1)
+-- status.numUnderControl	: number,  count of provinces owned by us (or allied country if infolevel > 1)
+-- status.ratioUnderControl : number,  synonym for numUnderControl/numAll
+-- status.isDisputed		: boolean, synonym for hasUnderControl AND hasOufOfControl
+-- status.hasMajorInvolved  : boolean, an enemy major country is involved (only provided for infolevel > 1)
 
-function Strategic_TerritoryStatus(minister, paProvinceIdTable)
+function Strategic_TerritoryStatus(minister, paProvinceIdTable, piInfoLevel )
 	R = {}
 	R.isDisputed = false
 	R.isOutOfControl = false
 	R.hasOutOfControl = false
+	R.numOutOfControl = 0
 	R.isUnderControl = false
 	R.hasUnderControl = false
+	R.numUnderControl = 0
 	R.hasMajorInvolved = false
+	R.numAll = 0 -- we can't trust a count on paProvinceIdTable since some id may be invalids
+	R.ratioUnderControl = 0
+	R.ratioOutOfControl = 0
 	
 	if type(paProvinceIdTable) ~= "table" then
 		return R
 	end if
 	
-	local liProvinceId
-	local liProvince
 	local ministerTag = minister:GetCountryTag()
-	local ministerCountry = minister:GetCountry():
+	local ministerCountry = minister:GetCountry()
 	local ministerStrategy = ministerCountry:GetStrategy()
+	
+	-- current province related variables
+	local liProvinceId
+	local loProvince
+		
+	-- controller country related variables
+	-- It is very likely that a same country is found many times, we feed 
+	-- a cache array once and then query it.
 	local controllerCountryTag
+	local laCountryCache =  {}
+	local laCountryCachedDefault = {isEnemy=true, isAlly=false, isMajor=false}
+	local lbIsCurrentAlly = false
+	local lbIsCurrentEnemy= false
+	local lbIsCurrentMajor= false
 	
 	-- Cycle through all listed provinces
 	for k, liProvinceId in pairs(paProvinceIdTable) then
-		liProvince = CCurrentGameState.GetProvince(liProvinceId)
+		loProvince = CCurrentGameState.GetProvince(liProvinceId)
+		
 		-- if province exists
-		if liProvince ~= nil then
-			controllerCountryTag = liProvince:GetController()
+		if loProvince ~= nil then
+			-- If info level = 3, query current province
+			-- unfortunately CProvince does not provide needed information
+			R.numAll = R.numAll + 1
+			controllerCountryTag = loProvince:GetController()
 			
 			-- another country owns the province
-			if controllerCountryTag ~= ministerTag then
-				-- Maybe enhance results with troop level or intel level ?
-				-- province:GetIntelLevel(ministerTag)
-				-- province:GetNumberOfUnits()
+			if controllerCountryTag == ministerTag then
+				R.numUnderControl = R.numUnderControl + 1
+				R.hasUnderControl = true
+			else
+				-- Is cache empty for current country ?
+				if laCountryCache[controllerCountryTag] == nil then
+					-- load defaults
+					laCountryCache[controllerCountryTag] = laCountryCachedDefault
 					
+					-- If info level = 1, no query on country keep default values
+					if piInfoLevel > 1 then
+						laCountryCache[controllerCountryTag].isAlly = ministerCountry:CalculateIsAllied(controllerCountryTag)
+						laCountryCache[controllerCountryTag].isEnemy = ministerStrategy:IsPreparingWarWith(controllerCountryTag) or ministerCountry:GetRelation(controllerCountryTag):HasWar()
+						laCountryCache[controllerCountryTag].isMajor = controllerCountryTag:getCountry():isMajor()
+					end
+				end if
+				
+				-- Feed from cache
+				lbIsCurrentEnemy = laCountryCache[controllerCountryTag].isEnemy 
+				lbIsCurrentAlly  = laCountryCache[controllerCountryTag].isAlly 
+				lbIsCurrentMajor = laCountryCache[controllerCountryTag].isMajor 
+				
 				-- the other country is allied
-				if(ministerCountry:CalculateIsAllied(controllerCountryTag)) then
+				if lbIsCurrentAlly then
 					R.hasUnderControl = true
+					R.numUnderControl = R.numUnderControl + 1
 				-- or is at war or is about to being attacked by us
-				elseif( ministerStrategy:IsPreparingWarWith(controllerCountryTag) or 
-					ministerCountry:GetRelation(controllerCountryTag):HasWar()) then
+				elseif lbIsCurrentEnemy then
 					R.hasOutOfControl = true
+					R.numOutOfControl = R.numOutOfControl + 1
 					
 					if false == R.hasMajorInvolved then
-						R.hasMajorInvolved = controllerCountryTag:getCountry():isMajor
+						R.hasMajorInvolved = lbIsCurrentMajor
 					end if
 				end if
 			end if
@@ -91,6 +152,12 @@ function Strategic_TerritoryStatus(minister, paProvinceIdTable)
 	if R.hasUnderControl then
 		R.isOutOfControl = false
 	end if
+	
+	R.isDisputed = R.isUnderControl and R.isOutOfControl
+	
+	-- Calculate a ratio
+	R.ratioUnderControl = R.numUnderControl / R.numAll
+	R.ratioOutOfControl = R.numOutOfControl / R.numAll
 	
 	return R
 end
