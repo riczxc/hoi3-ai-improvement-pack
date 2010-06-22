e209e216677ac2fb61242cc5eab1e0cc6c4a0d13
guibod@free.fr <guibod@free.fr@2ee7eb82-aa46-11de-8882-bb5941c2e707>
2010-06-22 15:56:55 +0000

    - Some more implementation of save/load system on few more objects.

--- script/lib/hoi3/api/CAIProductionMinister.lua
+++ script/lib/hoi3/api/CAIProductionMinister.lua
@@ -6,33 +6,45 @@ CAIProductionMinister = CAIAgent:subclass('hoi3.CAIProductionMinister')
 -- @since 1.3
 -- @return number
 function CAIProductionMinister:CountEscortsUnderConstruction()
-	Hoi3Object.throwNotYetImplemented()
+	return self:loadResultOrFakeOrRandom(
+		'number',
+		'CountEscortsUnderConstruction'
+	)
 end
 
 ---
 -- @since 1.3
 -- @return number
 function CAIProductionMinister:CountTotalDesiredEscorts()
-	Hoi3Object.throwNotYetImplemented()
+	return self:loadResultOrFakeOrRandom(
+		'number',
+		'CountTotalDesiredEscorts'
+	)
 end
 
 ---
 -- @since 1.3
 -- @return number
 function CAIProductionMinister:CountTransportsUnderConstruction()
-	Hoi3Object.throwNotYetImplemented()
+	return self:loadResultOrFakeOrRandom(
+		'number',
+		'CountTransportsUnderConstruction'
+	)
 end
 
 ---
 -- @since 1.3
 -- @return number
 function CAIProductionMinister:GetDesperation()
-	Hoi3Object.throwNotYetImplemented()
+	return self:loadResultOrFakeOrRandom(
+		'number',
+		'GetDesperation'
+	)
 end
 
 ---
 -- @since 1.3
 -- @return void
 function CAIProductionMinister:PrioritizeBuildQueue()
-	Hoi3Object.throwUnknownSignature()
+	Hoi3Object.throwNotYetImplemented()
 end
